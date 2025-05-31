b161fbb57e1ea2852ddd3baab4d15b1544ab9b3a
n-jabes <nshutij7@gmail.com>
2025-05-31 12:42:17 +0200

    update: resolve deployement errors

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -543,6 +543,7 @@ export default function AdminUsersPage() {
                             onClick={() => {
                               setSelectedUser(user);
                               setRejectionReason(user.rejectionReason || '');
+                              console.log(rejectionReason)
                             }}
                           >
                             View
